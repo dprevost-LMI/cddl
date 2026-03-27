8655be181b0a5db7224225a490e39d20d560c042
dprevost-perso <dprevost-github@outlook.com>
2026-03-26 20:50:53 -0400

    ci: add author to release notes

--- .release-it.base.ts
+++ .release-it.base.ts
@@ -1,4 +1,8 @@
 import type { Config } from 'release-it';
+import type { Commit } from 'conventional-commits-parser';
+import type { Options as WriterOptions } from 'conventional-changelog-writer';
+import type * as GitRawCommits from 'git-raw-commits';
+import type { Config as PresetConfig } from 'conventional-changelog-config-spec';
 
 const baseConfig = (name: string): Config => ({
   plugins: {
@@ -25,15 +29,31 @@ const baseConfig = (name: string): Config => ({
           { type: "revert", section: "🏡 Other Changes" },
           { type: "ops", section: "🏡 Other Changes" },
         ],
-      },
+      } satisfies PresetConfig & { name: string },
       // This maps the breaking change (eg. `!` like `feat!: ...`) section to your custom title
       presetConfig: {
-         header: "🚨 Breaking Changes"
-      },
+        header: "🚨 Breaking Changes",
+        issueUrlFormat: "https://github.com{{host}}/{{owner}}/{{repository}}/issues/{{id}}",
+        commitUrlFormat: "https://github.com{{host}}/{{owner}}/{{repository}}/commit/{{hash}}",
+        compareUrlFormat: "https://github.com{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}",
+        userUrlFormat: "https://github.com{{user}}"
+      } satisfies PresetConfig,
+      writerOpts: {
+          // 2. This helper function is what allows the author to appear
+          includeDetails: true,
+          transform: (commit, _context) => {
+          // If the commit has a known author, append it to the subject
+          if (commit.authorName || commit.committerName) {
+              const author = commit.authorName || commit.committerName;
+              commit.subject = `${commit.subject} by @${author}`;
+          }
+          return commit;
+          }
+      } satisfies WriterOptions<Commit>,
       // Path filtering that changelogithub does not support for scoped release notes per package.
       gitRawCommitsOpts: {
         path: [".", "../../tsconfig.json"]
-      }
+      } satisfies GitRawCommits.GitOptions
     }
   },
   git: {
